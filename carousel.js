474031b0af09f5b1502c0cebda48b63ff0e8c265
WebWitch <thewebwitchproject@gmail.com>
2026-04-11 23:01:03 -0700

    Add SVG assets for advertisements and update carousel functionality to support light and dark modes

--- carousel.js
+++ carousel.js
@@ -1,32 +1,46 @@
 // Tab content data
-const ADS_CONTENT = [
+const LIGHT_ADS_CONTENT = [
   {
     title: 'FREE PSYCHIC READING',
     description: 'See what the universe is trying to show you today!',
     cta: 'REVEAL YOUR FUTURE',
     svg: 'assets/CrystalBall.svg',
-    template: 'template-1',
-    mode: 'light',
   },
   {
     title: "You've Got Mail!",
     description: 'Get Your Mail Anytime, Anywhere.',
     cta: 'View Inbox',
     svg: 'assets/Envelope.svg',
-    template: 'template-2',
-    mode: 'light',
   },
   {
     title: 'PLAY FOR FREE',
     description: 'Fast rounds. Easy State. No waiting around.',
     cta: 'START GAME',
     svg: 'assets/Gaming.svg',
-    template: 'template-2',
-    mode: 'light',
   },
 ];
 
-const LENGTH = ADS_CONTENT.length;
+const DARK_ADS_CONTENT = [
+  {
+    title: 'GREEN MEAT IS THE FUTURE',
+    description: 'No questions asked. Just flavor.',
+    cta: 'TRY TODAY',
+    svg: 'assets/Cow.svg',
+  },
+  {
+    title: 'SYSTEM NOTICE',
+    description: 'This session has been flagged for unusual viewing patterns',
+    cta: 'Read More',
+    svg: 'assets/ExclamationTriangle.svg',
+  },
+  {
+    title: "YOU'RE BEING RECORDED",
+    description: '',
+    cta: 'ABORT',
+  },
+];
+
+const LENGTH = LIGHT_ADS_CONTENT.length;
 
 export const initCarousel = () => {
   let currentIndex = 0;
@@ -53,13 +67,28 @@ export const initCarousel = () => {
 // Helper function to update ad content based on the current index
 const updateAdContent = (index) => {
   const ad = document.querySelector('.advertisement');
-  const { title, description, cta } = ADS_CONTENT[index];
+
+  let adData = LIGHT_ADS_CONTENT[index];
+
+  // check if light mode
+  const isLightMode =
+    document.documentElement.getAttribute('data-theme') === 'light';
+
+  if (!isLightMode) {
+    adData = DARK_ADS_CONTENT[index];
+  }
+
+  const { title, description, cta, svg } = adData;
   ad.querySelector('.ad-title').textContent = title;
   ad.querySelector('.ad-description').textContent = description;
   ad.querySelector('.ad-cta').textContent = cta;
 
   const adSvgElements = ad.querySelectorAll('.ad-svg');
   adSvgElements.forEach((element) => {
-    element.style.maskImage = `url(${ADS_CONTENT[index].svg})`;
+    if (svg) {
+      element.style.maskImage = `url(${svg})`;
+    } else {
+      element.style.maskImage = '';
+    }
   });
 };
