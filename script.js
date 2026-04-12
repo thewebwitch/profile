aa54a9aa8a0878d40965fe7cc5c0f2c6d9d7f7e3
WebWitch <thewebwitchproject@gmail.com>
2026-04-12 14:34:34 -0700

    Add avatar images and implement worm effect on mouse movement; update home tab layout and styles

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { initCarousel } from './carousel.js';
 import { initTabs } from './tabs.js';
 import { initTheme } from './themeToggle.js';
 import './interactiveHeader.js';
+import './worm.js';
 
 // Initialize on page load
 initTheme();
