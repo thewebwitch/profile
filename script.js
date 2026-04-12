da5cae8d1ff47b51e43fb9ed23b8f5f544ac97aa
WebWitch <thewebwitchproject@gmail.com>
2026-04-12 16:14:16 -0700

    Remove worm effect implementation and associated styles

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ import { initCarousel } from './carousel.js';
 import { initTabs } from './tabs.js';
 import { initTheme } from './themeToggle.js';
 import './interactiveHeader.js';
-import './worm.js';
 
 // Initialize on page load
 initTheme();
